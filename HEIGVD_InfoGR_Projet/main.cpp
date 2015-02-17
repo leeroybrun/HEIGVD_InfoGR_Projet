87f1b1ea9a0391ce9e9200a3cbd13c49099965b1
Leeroy Brun <leeroy.brun@gmail.com>
2015-02-17 19:39:21 +0100

    Initial Commit

---
+++ HEIGVD_InfoGR_Projet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HEIGVD_InfoGR_Projet
+//
+//  Created by Leeroy Brun on 17.02.15.
+//  Copyright (c) 2015 Leeroy Brun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
